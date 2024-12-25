e3f2701b7457145f3e4f7ac3af7c61ddad548c3d
vishal-netcore <netcore.vishal@gmail.com>
2024-12-25 13:36:08 +0530

    dev

--- modules/logger.py
+++ modules/logger.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def load_path():
     with open("configs/config.yaml", "r") as file:
         config = yaml.safe_load(file)
-        log_path = config.get("log_path", "/logs/rebuild.log")
+        log_path = config.get("log_path", "./logs/rebuild.log")
         log_path += "rebuild_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".log"
         print('log__path', log_path)
         return log_path
