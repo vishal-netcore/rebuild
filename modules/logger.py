7c3f52d278448d470adb12674b91634acd65f30d
vishal-netcore <netcore.vishal@gmail.com>
2024-12-25 12:22:19 +0530

    dev

--- modules/logger.py
+++ modules/logger.py
@@ -6,11 +6,12 @@ def load_path():
     with open("configs/config.yaml", "r") as file:
         config = yaml.safe_load(file)
         log_path = config.get("log_path", "/logs/rebuild.log")
+        log_path += "_" + str(datetime.now()) + ".log"
         print('logpath', log_path)
         return log_path
 
 logging.basicConfig(
-    filename=load_path() + "_" + str(datetime.now()) + ".log",
+    filename=load_path() ,
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
