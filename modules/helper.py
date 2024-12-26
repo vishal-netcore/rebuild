c09519412a2b950617e079589e69c76281ec541f
vishal-netcore <netcore.vishal@gmail.com>
2024-12-26 12:59:42 +0530

    dev

--- modules/helper.py
+++ modules/helper.py
@@ -390,7 +390,7 @@ def change_priority(servers, client, target_id):
         for config_member in config_members:
             for status_member in status_members:
                 if status_member['name'] == config_member['host']:
-                    print(status_member['name'], status_member)
+                    print(status_member['name'], status_member['stateStr'])
         return
         lowest_priority_member = None
         lowest_priority = float('inf')
